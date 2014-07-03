48914c51787acc237f12a4ff5e7aaf30c2c388fd
Ginna <bakerv07@yahoo.com>
2014-07-03 17:36:41 -0400

    A few other files
    Made a SASS file, also.  Gmapsinit is no longer useful.

--- routes/gmapsInit.js
+++ routes/gmapsInit.js
@@ -1,14 +1,15 @@
-function initialize_gmaps() {
+function initialize() {
 
   // initialize new google maps LatLng object
-  var myLatlng = new google.maps.LatLng(40.705786,-74.007672);
-
-  // set the map options hash
-  var mapOptions = {
-    center: myLatlng,
-    zoom: 16,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
 
   // get the maps div's HTML obj
   var map_canvas_obj = document.getElementById("map-canvas");
@@ -25,7 +26,3 @@ function initialize_gmaps() {
   // Add the marker to the map by calling setMap()
   marker.setMap(map);
 }
-
-$(document).ready(function() {
-  initialize_gmaps();
-});
